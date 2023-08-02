1ffe4e9169ea160e4cb93f4ce46d94109a972b54
Afnan <alateef4@gmail.com>
2023-08-01 21:46:44 -0400

    Creates simple navigation menu.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Homepage from './components/Homepage';
+import ProjectsPage from './components/ProjectsPage';
+import BlogsPage from './components/BlogsPage';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/projects">Projects</Link>
+              </li>
+              <li>
+                <Link to="/blogs">Blogs</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/projects">
+              <ProjectsPage />
+            </Route>
+            <Route path="/blogs">
+              <BlogsPage />
+            </Route>
+            <Route path="/">
+              <Homepage />
+            </Route>
+          </Routes>
+        </div>
+      </Router>
   );
-}
+};
 
 export default App;
