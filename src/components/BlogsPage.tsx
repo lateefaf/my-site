cbfa46c8f03090e1989e01c8256394661bbd84e2
Afnan <alateef4@gmail.com>
2023-08-01 21:23:35 -0400

    Creates components directory and 3 pages.

---
+++ src/components/BlogsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BlogsPage = () => {
+    return (
+        <div>
+            <h1>My Blogs</h1>
+            <p>Here, you'll find all of my blogs.</p>
+        </div>
+    );
+};
+
+export default BlogsPage;
---
+++ src/components/ProjectsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProjectsPage = () => {
+    return (
+        <div>
+            <h1>My Projects</h1>
+            <p>Here, you'll find all of my projects.</p>
+        </div>
+    );
+};
+
+export default ProjectsPage;
